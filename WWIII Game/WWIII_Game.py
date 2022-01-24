5795683aa61e2c52ec21e7aa039039bbb6d26a74
Banr <61343370+B4nr@users.noreply.github.com>
2022-01-24 02:01:23 -0300

    mainfiles

---
+++ WWIII Game/WWIII_Game.py
@@ -0,0 +1,170 @@
+print("Instructions: Type the options, hit enter and see what your fate is!")
+print("The year is 2077, the WW3 has just ended. Global radiation levels are insane.\n")
+print("Houses are the rarest thing in the world now because without them, the radiation will kill you!\nGood luck!\n")
+switzerlandflag = ('''
+   __
+  <__>
+   ||______________________________
+   ||#######################JGS####|
+   ||##############################|
+   ||#############    #############|
+   ||#############    #############|
+   ||#########            #########|
+   ||#########            #########|
+   ||#############    #############|
+   ||#############    #############|
+   ||##############################|
+   ||##############################|
+   ||~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   ||
+   || 
+   ||
+   ||                   Welcome to the country of Switzerland. Do What's right, or die!
+    ''')
+skull = ('''
+                              __xxxxxxxxxxxxxxxx___.
+                        _gxXXXXXXXXXXXXXXXXXXXXXXXX!x_
+                   __x!XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!x_
+                ,gXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx_
+              ,gXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!_
+            _!XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!.
+          gXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXs
+        ,!XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!.
+       g!XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
+      iXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
+     ,XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx
+     !XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx
+   ,XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx
+   !XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXi
+  dXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!                                                                                                           
+  ,ad8888ba,                                                ,ad8888ba,                                      
+ d8"'    `"8b                                              d8"'    `"8b                                     
+d8'                                                       d8'        `8b                                    
+88            ,adPPYYba, 88,dPYba,,adPYba,   ,adPPYba,    88          88 8b       d8  ,adPPYba, 8b,dPPYba,  
+88      88888 ""     `Y8 88P'   "88"    "8a a8P_____88    88          88 `8b     d8' a8P_____88 88P'   "Y8  
+Y8,        88 ,adPPPPP88 88      88      88 8PP"""""""    Y8,        ,8P  `8b   d8'  8PP""""""" 88          
+ Y8a.    .a88 88,    ,88 88      88      88 "8b,   ,aa     Y8a.    .a8P    `8b,d8'   "8b,   ,aa 88          
+  `"Y88888P"  `"8bbdP"Y8 88      88      88  `"Ybbd8"'      `"Y8888Y"'       "8"      `"Ybbd8"' 88          
+                                                                                                            
+                                                                                                            
+  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
+  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
+  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
+  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
+  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
+  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+  !XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+   XXXXXXXXXXXXXXXXXXXf~~~VXXXXXXXXXXXXXXXXXXXXXXXXXXvvvvvvvvXXXXXXXXXXXXXX!
+   !XXXXXXXXXXXXXXXf`       'XXXXXXXXXXXXXXXXXXXXXf`          '~XXXXXXXXXXP
+    vXXXXXXXXXXXX!            !XXXXXXXXXXXXXXXXXX!              !XXXXXXXXX
+     XXXXXXXXXXv`              'VXXXXXXXXXXXXXXX                !XXXXXXXX!
+     !XXXXXXXXX.                 YXXXXXXXXXXXXX!                XXXXXXXXX
+      XXXXXXXXX!                 ,XXXXXXXXXXXXXX                VXXXXXXX!
+      'XXXXXXXX!                ,!XXXX ~~XXXXXXX               iXXXXXX~
+       'XXXXXXXX               ,XXXXXX   XXXXXXXX!             xXXXXXX!
+        !XXXXXXX!xxxxxxs______xXXXXXXX   'YXXXXXX!          ,xXXXXXXXX
+         YXXXXXXXXXXXXXXXXXXXXXXXXXXX`    VXXXXXXX!s. __gxx!XXXXXXXXXP
+          XXXXXXXXXXXXXXXXXXXXXXXXXX!      'XXXXXXXXXXXXXXXXXXXXXXXXX!
+          XXXXXXXXXXXXXXXXXXXXXXXXXP        'YXXXXXXXXXXXXXXXXXXXXXXX!
+          XXXXXXXXXXXXXXXXXXXXXXXX!     i    !XXXXXXXXXXXXXXXXXXXXXXXX
+          XXXXXXXXXXXXXXXXXXXXXXXX!     XX   !XXXXXXXXXXXXXXXXXXXXXXXX
+          XXXXXXXXXXXXXXXXXXXXXXXXx_   iXX_,_dXXXXXXXXXXXXXXXXXXXXXXXX
+          XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXP
+          XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
+           ~vXvvvvXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXf
+                    'VXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXvvvvvv~
+                      'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX~
+                  _    XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXv`
+                 -XX!  !XXXXXXX~XXXXXXXXXXXXXXXXXXXXXX~   Xxi
+                  YXX  '~ XXXXX XXXXXXXXXXXXXXXXXXXX`     iXX`
+                  !XX!    !XXX` XXXXXXXXXXXXXXXXXXXX      !XX
+                  !XXX    '~Vf  YXXXXXXXXXXXXXP YXXX     !XXX
+                  !XXX  ,_      !XXP YXXXfXXXX!  XXX     XXXV
+                  !XXX !XX           'XXP 'YXX!       ,.!XXX!
+                  !XXXi!XP  XX.                  ,_  !XXXXXX!
+                  iXXXx X!  XX! !Xx.  ,.     xs.,XXi !XXXXXXf
+                   XXXXXXXXXXXXXXXXX! _!XXx  dXXXXXXX.iXXXXXX
+                   VXXXXXXXXXXXXXXXXXXXXXXXxxXXXXXXXXXXXXXXX!
+                   YXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXV
+                    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
+                    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXf
+                       VXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXf
+                         VXXXXXXXXXXXXXXXXXXXXXXXXXXXXv`
+                          ~vXXXXXXXXXXXXXXXXXXXXXXXf`
+                              ~vXXXXXXXXXXXXXXXXv~
+                                 '~VvXXXXXXXV~~
+                                       ~~
+    ''')
+bear = ('''
+     .'"'.        ___,,,___        .'``.
+: (\  `."'"```         ```"'"-'  /) ;
+ :  \                         `./  .'
+  `.                            :.'
+    /        _         _        \
+   |         0}       {0         |
+   |         /         \         |
+   |        /           \        |
+   |       /             \       |
+    \     |      .-.      |     /
+     `.   | . . /   \ . . |   .'
+       `-._\.'.(     ).'./_.-'
+           `\'  `._.'  '/'
+             `. --'-- .'
+               `-...-'
+    ''')
+
+print("Which country do you want to be escape to?\nOptions:\nUK\nCanada\nSwitzerland\nBrazil\n")
+country = input("Type in your option: ")
+country = country.lower()
+
+if(country=="switzerland"):
+    print(switzerlandflag)
+    print("It's been one year since you've moved to Switzerland. You have found home, and you managed to survive.")
+elif(country=="brazil"):
+    print(skull)
+    print("Due to the raise of the ocean levels, Brazil's coast has been flooded, causing the population to go to the center and a shortage of houses.")
+    print("You tried to fight for a house, but you were unfortunately killed in a fight against a Jiu Jitsu fighter.")
+    quit()
+elif(country=="canada"):
+    print(skull)
+    print("There was a blizzard on your way to Canada. Your plane lost control and crashed. You could not survive the hard crash.")
+    quit()
+elif(country=="uk"):
+    print("Due to the raise of the ocean levels, UK has been completely flooded. You drowned")
+    quit()
+
+print("\n3 years later...\nThings went bad. A wild brown bear has appeared. How do you defend against it?")
+print(bear)
+print("Options:\nKnife\nShotgun\nRun")
+defense = input("\nType in your option:")
+defense = defense.lower()
+
+if(defense=="knife"):
+    print("You managed to throw the knife at the bear head kill it. You're safe for now.\n")
+elif(defense=="shotgun"):
+    print(skull)
+    print("You weren't careful enough and the shotgun wasn't loaded. The bear killed you.")
+    quit()
+elif(defense=="run"):
+    print(skull)
+    print("You tried to run, but you fell on the floor and the bear devoured you.")
+    quit()
+
+print("15 years later...\nYou're in your beathbed. You've went through a lot and you're strong. Everyone is going to miss you. Congratulations for your life!")
+print('''
+            ___________
+            '._==_==_=_.'
+            .-\:      /-.
+           | (|:.     |) |
+            '-|:.     |-'
+              \::.    /
+               '::. .'
+                 ) (
+               _.' '._
+              `"""""""`
+''')
